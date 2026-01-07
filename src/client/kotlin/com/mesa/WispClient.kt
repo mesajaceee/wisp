5d48bc37033ffbac50257c8cdaa3d1605d358723
Javokhir Matnazarov <fracta7@gmail.com>
2026-01-07 21:14:36 +0500

    init

---
+++ src/client/kotlin/com/mesa/WispClient.kt
@@ -0,0 +1,9 @@
+package com.mesa
+
+import net.fabricmc.api.ClientModInitializer
+
+object WispClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/com/mesa/Wisp.kt
@@ -0,0 +1,15 @@
+package com.mesa
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object Wisp : ModInitializer {
+    private val logger = LoggerFactory.getLogger("wisp")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("wisp is loaded")
+	}
+}
